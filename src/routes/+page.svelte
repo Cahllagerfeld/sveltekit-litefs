d897f8748549c64c443346d1f69d44b86bfc2892
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-04-13 13:56:54 +0000

    fix: update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>This is a very basic example on how to use fly.io with Sveltekit</p>
