e810cedaeedea97f6413affa311af02e0ca4bf1d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-04-15 13:59:18 +0000

    feat: link to litefs docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>
-	This is a very basic example on how to use <a href="https://fly.io/">fly.io</a> with Sveltekit
+	This is a very basic example on how to use <a href="https://fly.io/">fly.io's</a>
 </p>
+<p>Litefs with Sveltekit. See <a href="https://fly.io/docs/litefs/">the docs for Litefs</a></p>
